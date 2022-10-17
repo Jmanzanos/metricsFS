b5b5ebcc74b3e14297b606ca5876a81b38e16a69
Jorge Manzano <jmanzanow@live.com>
2022-10-16 23:20:43 -0300

    added view folder inside app with card-index

---
+++ app/View/Components/card-index.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\View\Components\Layouts;
+use Illuminate\View\Component;
+class App extends Component
+{
+    public function render()
+    {
+        return view('Components.card-index');
+    }
+}
